5c1acd24dc4885e272f69d729286822d40a48e16
Peter Hurford <peter@peterhurford.com>
2015-04-28 00:40:38 -0500

    Started list problems in R

---
+++ lists/lists.R
@@ -0,0 +1,47 @@
+### 1.01 Find the last element of a list.
+last <- function(ll) unlist(tail(ll, 1))
+last(list("a", "b", "c", "d"))
+# [1] "d"
+
+### 1.02 Find the last but one element of a list.
+almost_last <- function(ll) ll[length(ll) - 1]
+almost_last(list("a", "b", "c", "d"))
+# [1] "c"
+
+### 1.03 Find the K'th element of a list.
+### The first element in the list is number 1.
+kth <- function(ll, k) ll[[k]]
+kth(list("a", "b", "c", "d"), 2)
+# [1] "b"
+
+### 1.04 Find the number of elements of a list.
+length(list("a", "b", "c", "d"))
+# [1] 4
+
+### 1.05 Reverse a list.
+rev(list("a", "b", "c", "d"))
+
+### 1.06 Find out whether a list is a palindrome.
+is.palindrome <- function(ll) identical(rev(ll), ll)
+is.palindrome(list("n", "o"))
+# [1] FALSE
+is.palindrome(list("a", "b", "b", "a"))
+# [1] TRUE
+
+### 1.07 Flatten a nested list structure.
+flatten <- function(ll) list(unlist(ll))
+flatten(list("a", list("b", list("c", "d"))))
+# [[1]]
+# [1] "a" "b" "c" "d"
+
+### 1.08 Eliminate consecutive duplicates of list elements.
+compress <- function(ll) {
+  ol <- list()
+  for (i in seq_along(ll)) {
+    if (i == length(ll)) { ol <- c(ol, ll[[i]]) }
+    else if (ll[[i]] != ll[[i+1]]) { ol <- c(ol, ll[[i]]) }
+  }
+  ol
+} #TODO: Is this really the most efficient implementation?
+compress(list("a", "a", "a", "a", "b", "c", "c", "a", "a", "d", "e", "e", "e", "e"))
+# list("a", "b", "c", "a", "d", "e")
